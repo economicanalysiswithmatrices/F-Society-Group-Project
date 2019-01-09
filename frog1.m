be5de7009dc479c77a9df74a9139310a5c91a6c3
ALexJLai <44679668+ALexJLai@users.noreply.github.com>
2019-01-09 20:14:37 +0000

    Update frog1.m
    Cars move as individuals. Need to add collision.

--- frog1.m
+++ frog1.m
@@ -1,115 +1,59 @@
 
 function frog1()
 
-
-
-
-
-
+  
 fig1 = figure('KeyPressFcn',@presskeys)
 rectangle('position',[0 0 10 5]) %big boundry
-
-
-
 rectangle('position',[0 0 10 1],'facecolor',[0 0.5,0])%lanes
-
 rectangle('position',[0 1 10 1],'facecolor','k')
-
 rectangle('position',[0 2 10 1],'facecolor','k')
-
 rectangle('position',[0 3 10 1],'facecolor','k')
-
 rectangle('position',[0 4 10 1],'facecolor','g')
+axis off
 
-axis equal
-
-
-
-
+over = 0
+grid = zeros(10,10)
 
 xRana = 4.5+ 15.*[0.0 0.4 0.4 0.2 0.4 0.6 0.8 0.6 0.6 1.0 0.8  1.0 0.8 0.7 0.9 0.8 0.9   0.83 0.8 0.7 0.3 0.2   0.16 0.1 0.2 0.1 0.3 0.2 0.0 0.2]/16;
-
 yRana = 15.*[0.0 0.0 0.1 0.4 0.1 0.1 0.4 0.1 0.0 0.0 0.1  0.4 0.5 0.4 0.6 0.8 0.9   0.93 1.0 0.9 0.9 1.0   0.93 0.9 0.8 0.6 0.4 0.5 0.4 0.1]/16;
-
 cow = patch(xRana,yRana,'y','edgecolor','b');
-
 %cow = rectangle('position',[4.5 0 1 1],'facecolor','w');%character
-
 poscowX = get(cow,'XData')%searching for position of cow on the board
-
 poscowY = get(cow,'YData')
-
-
-
-
-
-
-
-
-
-
-
-    function presskeys(varargin)
-        
-        poscowX = get(cow,'XData')%searching for position of cow on the board
-        
-        poscowY = get(cow,'YData')
-        
-        display(poscowX)
-        
+    
+    function presskeys(varargin)      
+        poscowX = get(cow,'XData')%searching for position of cow on the board        
+        poscowY = get(cow,'YData')        
+        display(poscowX)        
         display(poscowY)
-        
-        
-        
+                        
         switch varargin{2}.Key
-            
             case 'uparrow'
-                
-                if poscowY<4
-                    
+                if poscowY<4                    
                     poscowY = poscowY+1
-                    
+                  elseif poscowY(1)>1
+                    Over()
+                end                
+                                                              
+            case 'downarrow'                
+                if poscowY>0                    
+                    poscowY = poscowY-1                    
                 end
                 
-                
-                
-                
-                
-            case 'downarrow'
-                
-                if poscowY>0
-                    
-                    poscowY = poscowY-1
-                    
-                end
-                
-            case 'rightarrow'
-                
-                if poscowX <8.5
-                    
-                    poscowX = poscowX+1
-                    
-                end
-                
-                
-                
-            case 'leftarrow'
-                
-                if poscowX>1
-                    
-                    poscowX = poscowX-1
-                    
-                end
-                
-                
-                
-        end
-        
-        set(cow,'Ydata',poscowY,'XData',poscowX)
-        
-        
-        
+            case 'rightarrow'                
+                if poscowX <8.5                    
+                    poscowX = poscowX+1                    
+                end               
+                               
+            case 'leftarrow'                
+                if poscowX>1                    
+                    poscowX = poscowX-1                    
+                end                                                
+        end        
+        set(cow,'Ydata',poscowY,'XData',poscowX)                      
     end
+
+    
 
 axis on
 xlim([0 10])
@@ -125,59 +69,64 @@ car(3) = rectangle('position',[3 3 1.5 1],'facecolor', 'b');
 
 %  for n=1:length(r(:,1))
 while A==1
-    pause(0.001)
-    
-    for n = 1:length(car(:))
-        carpos1 = get(car(n),'position');
-        carpos1(1) = carpos1(1) + 0.05;
-     set(car(n),'position',carpos1);
-        drawnow
-        
-       
-    
-
-    distX = abs((carpos1(1)+carpos1(3)/2)-(poscowX(1)));
-    distY = abs(carpos1(2)-poscowY(1));
-    
-    if  distY<1/16 && distX<(carpos1(3)/2 + 1/60);
-        then clear fgr 
-          nx = n;
-    end
-    end
-     set(car(n),'position',carpos1);
+    pause(0.01)
+    carpos1 = get(car(1),'position');
+    carpos2 = get(car(2),'position');
+    carpos3 = get(car(3),'position');
      
-     if poscowY >= 4 
-        pause (1)
-         then clear fgr
-     end
+    if carpos1(1) > 10   
+        carpos1(1) =-2;
+        set(car(1),'position',carpos1);
+    
+    elseif carpos1(1) < 10
+        carpos1 = get(car(1),'position');
+        carpos1(1) = carpos1(1) + 0.03;
+        set(car(1),'position',carpos1);
+    end
+        
+    if carpos2(1) > 10   
+        carpos2(1) =-9;
+        set(car(2),'position',carpos2);
+        
+    elseif carpos2(1) < 10
+        carpos2 = get(car(2),'position');
+        carpos2(1) = carpos2(1) + 0.1;
+        set(car(2),'position',carpos2);       
+    end
+
+    if carpos3(1) > 10   
+        carpos3(1) =-2;
+        set(car(3),'position',carpos3);
+    
+    elseif carpos3(1) < 10
+        carpos3 = get(car(3),'position');
+        carpos3(1) = carpos3(1) + 0.05;
+        set(car(3),'position',carpos3);
+    end
+   
+    
+     %set(car(1),'position',carpos1);
+     
+    
+
+
 end
-
-
-
-
+    function youwin1()
+        youwin = text(0,3,{'You Win!'},'color','r','fontsize',80);
+        set(youwin,'visible','on');
+    end
+function Nokeys(~)
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+close(fig1)   
+function Over()
+    set(fig1,'KeyPressFcn',@Nokeys)
+    over = 1   
+    youwin1()
+    %close(fig1);
+    %disp("You win!")
+    pause(1.5)
+    close(fig1)
+end
+end
+        % xposcar1= get(car,'position')      
 
